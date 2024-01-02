a75d979cd6535199669fac251ade22a59bd8200b
Roman Mikhailov <rommik@gmail.com>
2024-01-02 19:58:59 +0000

    interface changed

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from langchain.schema import AIMessage, HumanMessage
 import openai
 import gradio as gr
 import os
-
-
+import random
+import time
 # os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
 
 # llm = ChatOpenAI(temperature=1.0, model='gpt-3.5-turbo-0613')
@@ -22,16 +22,53 @@ import os
 
 def greet(name):
     return "Hello " + name + "!"
+votes = []
+def add_vote(data: gr.LikeData):
+    if data.liked:
+        votes.append("You upvoted this response: " + data.value)
+    else:
+        votes.append("You downvoted this response: " + data.value)
+    print(data.value)
+
+def get_votes():
+    return '/n'.join(votes)
 
 with gr.Blocks() as demo:
+   
     gr.Markdown("# PMAI MVP")
     with gr.Tab("PM"):
-       gr.Markdown("Chat here")
+
+        chatbot = gr.Chatbot()
+       
+        msg = gr.Textbox()
+        clear = gr.Button("Clear")
+
+        def user(user_message, history):
+            return "", history + [[user_message, None]]
+
+        def bot(history):
+            bot_message = random.choice(["How are you?", "I love you", "I'm very hungry"])
+            history[-1][1] = ""
+            for character in bot_message:
+                history[-1][1] += character
+                time.sleep(0.05)
+                yield history
+
+        msg.submit(user, [msg, chatbot], [msg, chatbot], queue=False).then(
+            bot, chatbot, chatbot
+        )
+        chatbot.like(add_vote, None, None)
+        
+        clear.click(lambda: None, None, chatbot, queue=False)
 
     with gr.Tab("Project"):
         gr.Markdown("Project Info here")
     with gr.Tab("Memory"):
-        gr.Markdown("Assistant Memory")
+       gr.Markdown("Memory here")
+        
+        
+        
 
 if __name__ == "__main__":
+    demo.queue()
     demo.launch()
