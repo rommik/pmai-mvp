d2aa79d2faae8d5eb302ada682f1fe04acdd25d7
Roman Mikhailov <rommik@gmail.com>
2024-01-03 04:06:47 +0000

    enabled chat-gpt

--- app.py
+++ app.py
@@ -1,22 +1,24 @@
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import AIMessage, HumanMessage
+from dotenv import load_dotenv
 import openai
 import gradio as gr
 import os
 import random
 import time
-# os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+load_dotenv()
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
 
-# llm = ChatOpenAI(temperature=1.0, model='gpt-3.5-turbo-0613')
+llm = ChatOpenAI(temperature=1.0, model='gpt-3.5-turbo-0613')
 
-# def predict(message, history):
-#     history_langchain_format = []
-#     for human, ai in history:
-#         history_langchain_format.append(HumanMessage(content=human))
-#         history_langchain_format.append(AIMessage(content=ai))
-#     history_langchain_format.append(HumanMessage(content=message))
-#     gpt_response = llm(history_langchain_format)
-#     return gpt_response.content
+def predict(message, history):
+    history_langchain_format = []
+    for human, ai in history:
+        history_langchain_format.append(HumanMessage(content=human))
+        history_langchain_format.append(AIMessage(content=ai))
+    history_langchain_format.append(HumanMessage(content=message))
+    gpt_response = llm(history_langchain_format)
+    return gpt_response.content
 
 # gr.ChatInterface(predict).launch(auth=('user', 'password'))
 
@@ -30,43 +32,20 @@ def add_vote(data: gr.LikeData):
         votes.append("You downvoted this response: " + data.value)
     return votes
 
-def get_votes():
-    return '/n'.join(votes)
 
 with gr.Blocks() as demo:
    
     gr.Markdown("# PMAI MVP")
     with gr.Tab("PM"):
 
-        chatbot = gr.Chatbot()
+        chat = gr.ChatInterface(predict)
        
-        msg = gr.Textbox()
-        clear = gr.Button("Clear")
-
-        def user(user_message, history):
-            return "", history + [[user_message, None]]
-
-        def bot(history):
-            bot_message = random.choice(["How are you?", "I love you", "I'm very hungry"])
-            history[-1][1] = ""
-            for character in bot_message:
-                history[-1][1] += character
-                time.sleep(0.05)
-                yield history
-
-        msg.submit(user, [msg, chatbot], [msg, chatbot], queue=False).then(
-            bot, chatbot, chatbot
-        )
-        
-        
-        clear.click(lambda: None, None, chatbot, queue=False)
-
     with gr.Tab("Project"):
         gr.Markdown("Project Info here")
     with gr.Tab("Memory"):
        gr.Markdown("Memory here")
        votes_history = gr.Textbox(label="Votes")
-       chatbot.like(add_vote, None, outputs=votes_history)
+       chat.chatbot.like(add_vote, None, outputs=votes_history)
         
         
         
