6624ac223b6236beb00ed82de19b2059818357a8
Roman Mikhailov <rommik@gmail.com>
2024-01-03 21:23:48 +0000

    updated the model to chatgpt-4

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import time
 load_dotenv()
 os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
 
-llm = ChatOpenAI(temperature=1.0, model='gpt-3.5-turbo-0613')
+llm = ChatOpenAI(temperature=1.0, model='gpt-4')
 
 def predict(message, history):
     history_langchain_format = []
